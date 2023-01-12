778857c98f405da58f2fea9f6074fa764a0ac1ec
unknown <world2222222222@gmail.com>
2023-01-12 18:11:43 +0900

    feat: Implement route transition & webview code
    Route transition animation has changed from default to slide (right to left) animation. This animation happens when the 'GO' button at the bottom is pressed. And webview code is completed either.

---
+++ lib/webview.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class Webview extends StatefulWidget {
+  const Webview({Key? key}) : super(key: key);
+
+  @override
+  State<Webview> createState() => _WebviewState();
+}
+
+class _WebviewState extends State<Webview> {
+  /*
+  import 'package:flutter/services.dart';
+  -> Package for editing status bar
+
+  // Status bar - hiding
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [
+      SystemUiOverlay.bottom,
+    ]);
+  }
+
+  // Status bar - transparent
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
+  }
+  */
+
+  // Preparation for the webview
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse('https://hipspot.netlify.app/'));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
