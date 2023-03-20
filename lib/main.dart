57e2bbf5c8c79141b9d42197c435c7fc4c25f613
SangJun Lee <66112027+Sangjun-man@users.noreply.github.com>
2023-03-20 20:37:20 +0900

    Feat: 바텀네비게이션바 및 페이지 연동 구현 (#12)
    * feat: Implement route transition & webview code
    
    Route transition animation has changed from default to slide (right to left) animation. This animation happens when the 'GO' button at the bottom is pressed. And webview code is completed either.
    
    * feat: Implement route transition & webview code
    
    Route transition animation has changed from default to slide (right to left) animation. This animation happens when the 'GO' button at the bottom is pressed. And webview code is completed either.
    
    * feat: Implement route transition & webview code
    
    Route transition animation has changed from default to slide (right to left) animation. This animation happens when the 'GO' button at the bottom is pressed. And webview code is completed either.
    
    * style: Categorize files by class
    
    Created 'myhomepage.dart' and 'sliderightroute.dart' files for better
    readability and classification. And also deleted the unnecessary annotations.
    
    * feat: File merging & Implementing two way channel
    
    Merged flutter files and now when the 'go' button pressed, it shows up
    the webview page. Implemented two way channel between webveiw and
    javascript. Not sure if it works well for now so need to keep update
    with this function.
    
    * fix: Added unadded files
    
    Added some files that were missing after merging
    
    * fix: Solve conflicts
    
    Solved with some conflicts occured by merging from 'pubspec.lock'
    
    * fix: Solve conflicts
    
    Solved with some conflicts occured by merging from 'pubspec.lock'
    
    * feat : flutter - 웹뷰 JS 채널 연동 기능 구현
    
    * feat : Implement webview JS two way channel
    
    Implemented two way channel between webview and JS.
    
    * feat : Implement webview JS channel
    
    Implemented channel between webview and javascript. Not yet sure if it
    works well so needed to be inspected.
    
    * feat : 내 위치 찾기 기능 구현
    
    * feat: Implement BottomNavigationBar
    
    Implemented BottomNavigationBar and made it to be shown up when 'GO'
    button is tapped. Also, created three pages for webview, recommended
    page, and mypage. Made all the buttons to show each pages and used
    IndexedStack for preventing pages keep refreshing when other pages are
    shown up.
    
    * feat: Implement BottomNavigationBar
    
    Implemented BottomNavigationBar and made it to be shown up when 'GO'
    button is tapped. Also, created three pages for webview, recommended
    page, and mypage. Made all the buttons to show each pages and used
    IndexedStack for preventing pages keep refreshing when other pages are
    shown up.
    
    * feat: Implement BottomNavigationBar
    
    Implemented BottomNavigationBar and made it to be shown up when 'GO'
    button is tapped. Also, created three pages for webview, recommended
    page, and mypage. Made all the buttons to show each pages and used
    IndexedStack for preventing pages keep refreshing when other pages are
    shown up.
    
    Pushed on the branch 'issue-7' by mistake so pushing again on
    'issue-11'.
    
    * feat: Implement recommend page
    
    Implemented recommend page but still need to connect the API so that
    it can get show up the images and names of the cafes on each cards.
    
    * feat: Implement modal screen
    
    Implemented modal screen and it pops up when the 'mypage' button from
    the BottomNavigationBar is clicked. Also implemented a bit of API
    connection on 'recommend' page but still need to take a look more.
    
    * feat: Put ripple effect on login buttons
    
    Editted two login buttons, 'via_apple' and 'via_google', by using
    'Material' and 'Ink.image'.
    
    * fix: Change compileSdkVersion from 33 to 32
    
    Changed the compileSdkVersion from 33 to 32. You can check it in the
    'build.gradle' file.
    
    * Fix : unused file delete
    
    ---------
    
    Co-authored-by: unknown <world2222222222@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(extendBody: true, body: OnboardingScreen()),
+        home: Scaffold(
+            extendBody: true,
+            body: OnboardingScreen(),
+        ),
     );
   }
 }
---
+++ lib/screen/second_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:hipspot/component/Webview/webview.dart';
+import 'package:hipspot/screen/pages/mypage_screen.dart';
+import 'package:hipspot/screen/pages/recommend_screen.dart';
+
+import '../component/custom_modal.dart';
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    if (index == 2) { // index 2 represents the third item
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return const CustomModal();
+        },
+      );
+    } else {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: const [
+          Webview(),
+          RecommendScreen(),
+          MypageScreen(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Image.asset('assets/images/bottombar/map.png', width: 32, height: 32),
+            activeIcon: Image.asset('assets/images/bottombar/map_on.png', width: 32, height: 32),
+            label: '지도',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('assets/images/bottombar/recommend.png', width: 32, height: 32),
+            activeIcon: Image.asset('assets/images/bottombar/recommend_on.png', width: 32, height: 32),
+            label: '추천',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('assets/images/bottombar/mypage.png', width: 32, height: 32),
+            activeIcon: Image.asset('assets/images/bottombar/mypage_on.png', width: 32, height: 32),
+            label: '마이페이지',
+          )
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.black,
+      ),
+    );
+  }
+}
