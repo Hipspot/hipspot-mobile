89784f808247a87384c22f676669bc7efdd8d352
Sangjun-man <molung@icloud.com>
2023-05-08 10:16:33 +0900

    feat: 구글 로그인 진입지점 Url 수정

--- lib/component/login.dart
+++ lib/component/login.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web_auth/flutter_web_auth.dart';
 
+// 추후 const로 옮기기
+const APP_REDIRECT_URI = "hipspot-mobile";
+const GOOGLE_AUTH_URI_BASE = 'https://api.hipspot.xyz/auth/login/google';
+
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
 
@@ -63,11 +67,9 @@ class Login extends StatelessWidget {
                   fit: BoxFit.cover,
                   child: InkWell(
                     onTap: () async {
-                      const APP_REDIRECT_URI = "hipspot-mobile";
-
                       final result = await FlutterWebAuth.authenticate(
                         url:
-                            'https://api.hipspot.xyz/auth/login/google?redirect_uri=$APP_REDIRECT_URI',
+                            '$GOOGLE_AUTH_URI_BASE?platform=mobile', // 쿼리 형식으로 platform=mobile 이라는 값을 전달해주면, 서버에서 스케마에 맞게 자동 리다이렉트
                         callbackUrlScheme: APP_REDIRECT_URI,
                       );
                       final accessToken =
