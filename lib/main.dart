fec8f59a51d277b73643fd012aa92d75b6ec5059
Sangjun-man <molung@icloud.com>
2023-05-16 19:14:48 +0900

    feat: 리프레시토큰을 통한 엑세스토큰 재발급 이후 api 재요청 기능 구현
    - 엑세스토큰 재발급 과정
      엑세스토큰이 만료되면 서버는 리프레시토큰 유효성을 확인한뒤, 유효성이 확인된 경우
      statusCode 700번으로 body에 엑세스토큰, 리프레시토큰 전달하도록 설계
    
    - 재발급받은 엑세스토큰으로 api 재요청 과정
      dio에서 statusCode 700번의 경우 에러로 처리, 따라서 interceptor onError 메서드에서
      statusCode 700번 응답에 대해, 토큰 저장 및 기존 요청 url에 새로운 accessToken으로
      요청하도록 처리

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:dio/dio.dart';
-import 'package:dio_cookie_manager/dio_cookie_manager.dart';
-import 'package:cookie_jar/cookie_jar.dart';
+import 'package:hipspot/component/login.dart';
 import 'package:hipspot/splash_screen.dart';
 import 'package:hipspot/utils/api/interceptor/dio_interceptor.dart';
-import 'package:path_provider/path_provider.dart';
 
 final dio = Dio();
+late BuildContext context;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
   ));
-
-  // Get the application documents directory.
-  var dir = await getApplicationDocumentsDirectory();
-  // Create a FileStorage with the directory.
-  var storage = FileStorage(dir.path);
-  // Create a PersistCookieJar with the directory.
-  var cj = PersistCookieJar(storage: storage);
-  dio.interceptors.add(CookieManager(cj));
+  dio.interceptors.add(DioParseTokenInterceptor());
   dio.interceptors.add(DioAddQueryInterceptor());
   dio.interceptors.add(DioUnAuthErrorInterceptor());
-  cj.deleteAll();
   await dotenv.load();
   runApp(const MyApp());
 }
@@ -35,8 +26,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print(context);
     return const MaterialApp(
       home: SplashScreen(),
     );
   }
+}
+
+void showLoginModal(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (context) => const Login(),
+  );
 }
--- lib/utils/authenticate.dart
+++ lib/utils/authenticate.dart
@@ -8,26 +8,33 @@ import 'package:hipspot/main.dart';
 // 추후 const로 옮기기
 const APP_REDIRECT_URI = "hipspot-mobile";
 const GOOGLE_AUTH_URI_BASE = 'https://api.hipspot.xyz/auth/login/google';
+const DEV_AUTH_URI = 'https://api.hipspot.xyz/auth/login/dev';
 
 class Authenticate {
   static Future<bool> login() async {
-    const storage = FlutterSecureStorage();
     final _dio = dio;
 
     try {
       final result = await FlutterWebAuth.authenticate(
         url:
-            '$GOOGLE_AUTH_URI_BASE?platform=mobile', // 쿼리 형식으로 platform=mobile 이라는 값을 전달해주면, 서버에서 스케마에 맞게 자동 리다이렉트
+            '$DEV_AUTH_URI?platform=mobile', // 쿼리 형식으로 platform=mobile 이라는 값을 전달해주면, 서버에서 스케마에 맞게 자동 리다이렉트
         callbackUrlScheme: APP_REDIRECT_URI,
       );
+
+      print("auth $result");
 
       final String? accessToken =
           Uri.parse(result).queryParameters['access_token'];
       final String? refreshToken =
           Uri.parse(result).queryParameters['refresh_token'];
 
-      await storage.write(key: 'refreshToken', value: refreshToken);
-      await storage.write(key: 'accessToken', value: accessToken);
+      if (accessToken == null || refreshToken == null)
+        throw Exception('토큰이 없습니다.');
+
+      await Authenticate.writeTokens(
+        accessToken: accessToken!,
+        refreshToken: refreshToken!,
+      );
       dio.options.headers['Authorization'] = 'Bearer $accessToken';
     } catch (e) {
       print(e);
@@ -42,6 +49,20 @@ class Authenticate {
     try {
       storage.delete(key: 'refreshToken');
       storage.delete(key: 'accessToken');
+      print('로그아웃 완료');
+    } catch (e) {
+      print(e);
+      return false;
+    }
+    return true;
+  }
+
+  static Future<bool> writeTokens(
+      {required String accessToken, required String refreshToken}) async {
+    final storage = FlutterSecureStorage();
+    try {
+      await storage.write(key: 'refreshToken', value: refreshToken);
+      await storage.write(key: 'accessToken', value: accessToken);
     } catch (e) {
       print(e);
       return false;
