02fb8761e10e69a10672a216704837f0a2413486
world2222 <world2222222222@gmail.com>
2023-06-15 21:41:42 +0900

    refactor: Setting back the webview url
    Set the url in webview_screen.dart back as 'https://hipspot.xyz/'.

--- lib/screen/webview_screen.dart
+++ lib/screen/webview_screen.dart
@@ -18,7 +18,7 @@ class WebviewScreen extends StatefulWidget {
 class _WebviewScreenState extends State<WebviewScreen> {
   int loadingProgress = 0;
   late final WebViewController _controller = createWebViewController()
-    ..loadRequest(Uri.parse('https://deploy-preview-109--hipspot.netlify.app/'))
+    ..loadRequest(Uri.parse('https://hipspot.xyz/'))
     ..setNavigationDelegate(customedNavigationDelegate);
 
   @override
